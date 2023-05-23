aadd303888e614890a3ad0a110440ea9483eed9a
刘坤元 <liukunyuan-jk@360shuke.com>
2023-05-23 13:28:04 +0800

    解决死锁问题

--- test/com/shuke/demo/test/MicrometerTest.java
+++ test/com/shuke/demo/test/MicrometerTest.java
@@ -7,17 +7,11 @@ import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import io.micrometer.prometheus.PrometheusConfig;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.locks.LockSupport;
-import java.util.logging.LogManager;
 
 
 /**
@@ -26,10 +20,37 @@ import java.util.logging.LogManager;
  */
 public class MicrometerTest
 {
-    private static final Logger LOGGER = LoggerFactory.getLogger(MyAgentTest.class);
-
     public static void main( String[] args ) {
+        //组合注册表
+        CompositeMeterRegistry composite = new CompositeMeterRegistry();
+        //内存注册表
+        MeterRegistry registry = new SimpleMeterRegistry();
+        composite.add(registry);
+        //普罗米修斯注册表
+        PrometheusMeterRegistry prometheusRegistry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+        composite.add(prometheusRegistry);
+        //计数器
+        Counter compositeCounter = composite.counter("counter");
+        //计数
+        compositeCounter.increment();
+        try {
+            //暴漏8080端口来对外提供指标数据
+            HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+            server.createContext("/prometheus", httpExchange -> {
+                //获取普罗米修斯指标数据文本内容
+                String response = prometheusRegistry.scrape();
+                //指标数据发送给客户端
+                httpExchange.sendResponseHeaders(200, response.getBytes().length);
+                try (OutputStream os = httpExchange.getResponseBody()) {
+                    os.write(response.getBytes());
+                }
+            });
 
+
+            new Thread(server::start).start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
